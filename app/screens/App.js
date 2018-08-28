0272476c4d1bd8fcb1836c5991a823d667a78942
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-08-27 21:49:00 -0700

    text stubs for home page content

--- app/screens/App.js
+++ app/screens/App.js
@@ -10,40 +10,41 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React </Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instructions}>1. Pick a duration</Text>
+                <Text style={styles.component}>Timer wheel here</Text>
+                <Text style={styles.instructions}>2. Start the timer!</Text>
+                <Text style={styles.instructions}>Button here</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        // alignItems: 'flex-start',
+        backgroundColor: '#F5FCFF',
+    },
+    instructions: {
+        fontSize: 20,
+        textAlign: 'left',
+        margin: 10,
+    },
+    component: { // TODO: need timer wheel and button components!
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
 });
